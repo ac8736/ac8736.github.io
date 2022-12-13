d6fe44505bebf3ddc214c9afbcd08b1dcd5a1dce
Kewl <87680132+ac8736@users.noreply.github.com>
2022-12-13 15:08:36 -0500

    tests

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState, useEffect } from "react";
 
 function App() {
+  const first_text = "ac8736.github.io:-$ cd ac8736/portfolio";
+  const second_text = "console.log('hello')";
+  const [text1, setText1] = useState("");
+  const [text2, setText2] = useState("");
+
+  useEffect(() => {
+    let begin = 0;
+    const signature = setTimeout(() => {
+      setText1(first_text.slice(begin, text1.length + 1));
+    }, 10);
+    // if (text1.length === 20) {
+    //   clearTimeout(signature);
+    // }
+    // begin = text1.length;
+    // const directory = setTimeout(() => {
+    //   setText1(first_text.slice(begin, text1.length + 1));
+    // }, 10);
+
+    return () => clearTimeout(signature);
+  }, [text1]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>{text1}</div>
+      <div>{text2}</div>
     </div>
   );
 }
