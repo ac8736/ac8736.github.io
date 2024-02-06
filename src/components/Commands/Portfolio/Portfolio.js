223e9c4d528b090610c27a8f8476b214dd7783d1
Kewl <87680132+ac8736@users.noreply.github.com>
2024-02-05 21:37:42 -0500

    init

--- src/components/Commands/Portfolio/Portfolio.js
+++
@@ -1,9 +0,0 @@
-import "./Portfolio.css";
-
-export default function Portfolio() {
-  return (
-    <div>
-      <p>Portfolio</p>
-    </div>
-  );
-}
--- src/components/Unknown/Unknown.js
+++
@@ -1,5 +0,0 @@
-import "./Unknown.css";
-
-export default function Unknown({ log }) {
-  return <p>{log === "caret" ? "Caret color changed." : "Unknown command or in progress."}</p>;
-}
