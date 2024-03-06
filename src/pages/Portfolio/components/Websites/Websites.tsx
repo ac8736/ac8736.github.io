95a5f4eae2938340a4a9ee87269269ca4a54fd17
Kewl <87680132+ac8736@users.noreply.github.com>
2024-03-06 00:03:15 -0500

    adding website

--- src/pages/Portfolio/components/Websites/Websites.tsx
+++ src/pages/Portfolio/components/Websites/Websites.tsx
@@ -1,6 +1,7 @@
 import "./Websites.css";
 import { Fade } from "react-awesome-reveal";
 import SentimentAnalysisImage from "./assets/sentiment-analysis.png";
+import HelldiversCompanionImage from "./assets/hd-companion.jpg";
 
 export default function Websites() {
   return (
@@ -11,10 +12,6 @@ export default function Websites() {
             Sentiment Analysis |{" "}
             <a href="https://sentiment-analysis-app.netlify.app/" target="_blank" className="anchor-link">
               Link
-            </a>{" "}
-            |{" "}
-            <a href="https://github.com/ac8736/Sentiment-Analysis" target="_blank" className="anchor-link">
-              GitHub
             </a>
           </h2>
           <p className="text">Hugging Face Transformers, PyTorch, React</p>
@@ -30,6 +27,27 @@ export default function Websites() {
           <img src={SentimentAnalysisImage} className="sentiment-analysis-img" />
         </div>
       </Fade>
+      <div className="games-divider" />
+      <Fade triggerOnce={true}>
+        <div className="subcontent">
+          <h2>
+            Helldivers Companion |{" "}
+            <a href="https://main--helldivers-companion.netlify.app/" target="_blank" className="anchor-link">
+              Link
+            </a>
+          </h2>
+          <p className="text">SvelteKit, TypeScript</p>
+          <p className="text">
+            Using a community API, I created a web app to display information about the game Helldivers. The app displays all the
+            information provided, such as the sectors and each planet within them. Each planet can also open a modal, which will
+            display its liberation status and current owner. The webapp also includes a reactive search bar to allow users to
+            easily find planets they want to view.
+          </p>
+          <p className="text">SvelteKit with TypeScript is used to create the web app. The app is deployed on Netlify.</p>
+          <img src={HelldiversCompanionImage} className="sentiment-analysis-img" />
+        </div>
+      </Fade>
+      <div className="games-divider" />
     </div>
   );
 }
